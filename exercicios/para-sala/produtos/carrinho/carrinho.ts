1060dbd1d745cd7bd1fe947d16f193a9ed0bb189
thais maciel <thaismaciielf@hotmail.com>
2024-06-17 22:12:28 -0300

    adição dos arquivos para início da lógica para o carrinho de produtos

---
+++ exercicios/para-sala/produtos/carrinho/carrinho.ts
@@ -0,0 +1,9 @@
+import { CarrinhoProdutoInterface } from "./interfaces/carrinho-produto.interface";
+import { ProdutoInterface } from "../interfaces/produtos.interface";
+
+export class Carrinho {
+        private carrinhoProduto: CarrinhoProdutoInterface[]
+        display(...produtos: ProdutoInterface[]):void {
+
+    }
+}
